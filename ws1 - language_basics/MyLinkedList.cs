db01464b04ab4c0aa871cda834630c7c1e07c44d
liadraz <raz.liad@gmail.com>
2024-11-15 22:19:31 +0200

    Impl LinkedList class with several basic methods

---
+++ ws1 - language_basics/MyLinkedList.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ws1
+{
+    /*
+     *  Implenetation of a Single Linked List
+     * 
+     */
+    public class MyLinkedList
+    {
+        private class Node
+        {
+            public Node? next;
+            public object data;
+
+            public Node(object value)
+            {
+                next = null;
+                data = value;
+            }
+        }
+
+        private Node? head;
+        private Node? tail;
+        private Node? iterator;
+
+        private int counter;
+
+        public MyLinkedList()
+        {
+            // List will be constructed as follows
+            //          ITERATOR
+            //              V
+            //  HEAD ---> TAIL ---> NULL
+
+            head = new Node(0);
+            tail = new Node(0);
+
+            head.next = tail;
+            iterator = head;
+        }
+
+        public void Push(Object value)
+        {
+            Node toAdd = new Node(value);
+
+            if (iterator == null)
+            {
+                return;
+            }
+
+            iterator.next = toAdd;
+            iterator = toAdd;
+            iterator.next = tail;
+
+            counter++;
+        }
+
+        //Pop();
+
+        public object Peek()
+        {
+            if (iterator is not null)
+            {
+                return iterator.data;
+            }
+
+            return "NULL";
+        }
+
+        public bool IsEmpty()
+        {
+            return (counter == 0);
+        }
+
+        public int Count()
+        {
+            return counter;
+        }
+
+        //Clear();
+        //foreach - look how to iterate each element
+    }
+}
--- ws1 - language_basics/main.cs
+++ ws1 - language_basics/main.cs
@@ -1,18 +1,20 @@
 ﻿
 using ws1;
+using ws2;
 
 public class Test_Linked_List
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine($"Test Stack {Test_Stack()}");
+        //Console.WriteLine($"Test Stack {Test_Stack()}");
+        Console.WriteLine($"Test LinkedList {Test_LinkedList()}");
     }
 
     public static bool Test_Stack()
     {
         bool ret = false;
         const int STACK_SIZE = 3;
-        Stack stack = new Stack(STACK_SIZE);
+        MyStack stack = new MyStack(STACK_SIZE);
 
         Console.WriteLine("\t~ Test Stack ~ ");
 
@@ -60,6 +62,32 @@ public class Test_Linked_List
         {
             ret = true;
         }
+
+        return ret;
+    }
+
+    public static bool Test_LinkedList()
+    {
+        bool ret = false;
+
+        MyLinkedList ls = new MyLinkedList();
+
+        Console.WriteLine($"IsEmpty {ls.IsEmpty()}");
+
+        for (int i = 0; i < 4; i++)
+        {
+            ls.Push(i+10);
+            Console.WriteLine($"Data - {ls.Peek()}");
+            Console.WriteLine($"Data - {ls.Count()}");
+        }
+
+        // Pop() 
+        // Clear()
+
+        // Test Template
+        // func call.
+        // Check the return value
+        // print success
 
         return ret;
     }
