72462221e236028e1c85ae474b82f2899e876d41
liadraz <raz.liad@gmail.com>
2024-12-07 00:58:47 +0200

    Add complex shape class

---
+++ ws2 - Shapes_Hierarchy/ComplexShape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ws2___Shapes_Hierarchy
+{
+    public class ComplexShape
+    {
+        private List<Shape> compShape = new List<Shape>();
+
+        private double totalArea { get; set; }
+        private double totalPerimeter { get; set; }
+
+        public void Add(Shape shape)
+        {
+            totalArea += shape.Area();
+            totalPerimeter += shape.Perimeter();
+
+            compShape.Add(shape);
+        }
+
+        public double Area()
+        {
+            return totalArea;
+        }
+
+        public double Perimeter()
+        {
+            return totalPerimeter;
+        }
+    }
+}
